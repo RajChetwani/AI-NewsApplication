eabaa7a16055a1d1579767607bd514530c6c8cf4
rajd12 <2016.raj.chetwani@ves.ac.in>
2020-08-08 14:57:34 +0530

    Updates

---
+++ precache-manifest.418f92f36e9e7509af90ef98802efaa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc138cc811c4720ba72f0aed1180025",
+    "url": "/AI-NewsApplication/index.html"
+  },
+  {
+    "revision": "276a4ef291408d116ca8",
+    "url": "/AI-NewsApplication/static/css/main.ba57505a.chunk.css"
+  },
+  {
+    "revision": "627f37fafd471ce9fa3e",
+    "url": "/AI-NewsApplication/static/js/2.d174d206.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AI-NewsApplication/static/js/2.d174d206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276a4ef291408d116ca8",
+    "url": "/AI-NewsApplication/static/js/main.ada0a72a.chunk.js"
+  },
+  {
+    "revision": "fb461dd8ccfd0e93098f",
+    "url": "/AI-NewsApplication/static/js/runtime-main.63b9ec11.js"
+  },
+  {
+    "revision": "043b2689fc33d6100af6692445082bcc",
+    "url": "/AI-NewsApplication/static/media/newsimg.043b2689.jpg"
+  }
+]);
